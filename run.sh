d61288efdca982aeeff1dc21c596c3597a30f4ed
khehn <khehn@mail.uni-mannheim.de>
2017-11-16 00:02:43 +0100

    adapter execution env

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 ATT=${1:-"WARC-Record-ID"}
 INFILE=${2:-"hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz"}
 
-PYSPARK_PYTHON=$(readlink -f $(which python)) ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./NLTK/nltk_env/bin/python --master yarn --archives /home/wdps1703/nltk_env.zip#NLTK ner_module.py $ATT $INFILE
+PYSPARK_PYTHON=./NLTK/nltk_env/bin/python ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./NLTK/nltk_env/bin/python --master yarn --archives /home/wdps1703/nltk_env.zip#NLTK ner_module.py $ATT $INFILE

5af723488bbc6e7cd80b6932e01b994635b67b8a
khehn <khehn@mail.uni-mannheim.de>
2017-11-15 22:43:16 +0100

    changed tok to zip

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 ATT=${1:-"WARC-Record-ID"}
 INFILE=${2:-"hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz"}
 
-PYSPARK_PYTHON=$(readlink -f $(which python)) ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit --py-files nltk.zip --files tokenizers/*/* --master yarn ner_module.py $ATT $INFILE
+PYSPARK_PYTHON=$(readlink -f $(which python)) ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit --py-files nltk.zip --files tokenizers.zip --master yarn ner_module.py $ATT $INFILE

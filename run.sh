ba2d1116f2e412ba752dac9744fd4b98388c137a
khehn <khehn@mail.uni-mannheim.de>
2017-11-15 21:45:45 +0100

    put all files in one zip

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 ATT=${1:-"WARC-Record-ID"}
 INFILE=${2:-"hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz"}
 
-PYSPARK_PYTHON=$(readlink -f $(which python)) ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit --py-files nltk.zip, nltk_data.zip --master yarn ner_module.py $ATT $INFILE
+PYSPARK_PYTHON=$(readlink -f $(which python)) ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit --py-files shipData.zip --master yarn ner_module.py $ATT $INFILE

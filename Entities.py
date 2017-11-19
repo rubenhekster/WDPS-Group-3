51ceafb32bcaa46bb42d6e086c8e4885ec26068e
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2017-11-19 19:56:08 +0100

    Add files via upload

---
+++ Entities.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 19 17:47:26 2017
+
+@author: srema
+"""
+
+
+Entities = ["Obama", "Trump", "Clinton", "Bush", "Reagan"]
+
+
+for ent in range(len(Entities)):
+    print ('curl "http://10.149.0.127:9200/freebase/label/_search?q=' + Entities[ent] + '"')
+    

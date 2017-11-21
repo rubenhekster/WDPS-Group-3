871b8ce8713a268fadb0918e5054fe7907aa0ed1
khehn <khehn@mail.uni-mannheim.de>
2017-11-21 17:16:39 +0100

    adapter run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 ATT=${1:-"WARC-Record-ID"}
 INFILE=${2:-"hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz"}
 
-PYSPARK_PYTHON=/home/wdps1703/nltk_env/nltk_env/bin/python ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit --master yarn mainFile.py $ATT $INFILE
+PYSPARK_PYTHON=/home/wdps1703/bin/python ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit --master yarn mainFile.py $ATT $INFILE

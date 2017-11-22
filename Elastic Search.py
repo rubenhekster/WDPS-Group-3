af6977d8ce428f7467e560ae2fc26c8ae223bef9
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2017-11-22 20:13:54 +0100

    Add files via upload

--- Elastic Search.py
+++ Elastic Search.py
@@ -7,11 +7,13 @@ Created on Wed Nov 22 19:47:31 2017
 
 from elasticsearch import Elasticsearch
 
-Elasticsearch.download()
+#Elasticsearch.download()
 
 es = Elasticsearch("http://10.149.0.127:9200")
 
 #res = es.search(index="freebase", body={"query": {"match_all":{}}})
+
+query = ["Obama", "Trump", "Clinton", "Bush", "Reagan"]
 
 res = es.search(index="freebase", body={"query": {"match_all":{"obama"}}})
 

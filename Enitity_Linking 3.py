d46577589f421228266a4943a8adbcf904e23ab1
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2017-11-25 22:43:09 +0100

    Add files via upload

---
+++ Enitity_Linking 3.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 13:38:33 2017
+
+@author: Abdullah
+"""
+
+from elasticsearch import Elasticsearch
+from pyspark.context import SparkContext
+#from pyspark import SparkContext, SparkConf
+
+sc = SparkContext()
+
+print("Start")
+
+es = Elasticsearch("http://10.149.0.127:9200")
+
+rdd = SparkContext().parallelize["Obama", "Trump", "Clinton", "Bush", "Reagan"]
+
+#res = es.search(index="freebase", body={"query": {"match":{"label":x}}})
+
+output = rdd.flatMap(lambda x: es.search(index="freebase", body={"query": {"match":{"label":x}}}))
+
+print(output)
+
+print("Done")

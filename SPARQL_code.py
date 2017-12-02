2e0996908e0aebb4c7dc947bde89ea06bc2e6194
FImhaouran <f.imhaouran@gmail.com>
2017-12-02 16:58:05 +0100

    Update and rename SARQL_code.py to SPARQL_code.py

--- SPARQL_code.py
+++ SPARQL_code.py
@@ -1,12 +1,25 @@
 #SPARQL queries for selecting named enitities
 
+prefixes = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX fbase: <http://rdf.freebase.com/ns/>
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wds: <http://www.wikidata.org/entity/statement/>
+PREFIX wdv: <http://www.wikidata.org/value/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+"""
+
 
 #select person entity 
+"""
 SELECT  ?person 
 WHERE
 {
 	?person wdt:P31 wd:Q5 .       #where ?person isa(wdt:P31) human(wd:Q5)
 }
+"""
 
 #select organisation enitity 
 

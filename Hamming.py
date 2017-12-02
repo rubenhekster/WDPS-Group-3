2cb7e36bae76a5783f7f5656e0bdc1856c884496
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2017-12-02 17:30:51 +0100

    Hamming Distance

---
+++ Hamming.py
@@ -0,0 +1,31 @@
+import operator
+
+def Hamming(str1, str2):
+
+    str1_ = str1
+    str2_ = str2
+    
+    if str1 > str2:
+        dif = len(str1) - len(str2)
+        for i in range(dif):
+            str2_ = str2_ + "0"
+    elif str1 < str2:
+        dif = len(str2) - len(str1)
+        for i in range(dif):
+            str1_ = str1_ + "0"
+        
+    print(str1_, str2_)
+
+    ne = operator.ne
+    return sum(map(ne, str1_, str2_))
+
+def main():
+    str1 = "Obama"
+    str2 = ["Osama", "A lama", "Pyjama", "Futurama", "Alabama"]
+    
+    for i in range(len(str2)):
+        print("Hamming Distance: ", Hamming(str1, str2[i]))
+
+if __name__ == '__main__':
+    main()
+    print("Done")

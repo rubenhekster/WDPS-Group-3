9eca8873adea127804d880131a6d1a4fc66445f5
FImhaouran <f.imhaouran@gmail.com>
2017-12-02 16:59:39 +0100

    Update SPARQL_code.py

--- SPARQL_code.py
+++ SPARQL_code.py
@@ -22,18 +22,20 @@ WHERE
 """
 
 #select organisation enitity 
-
+"""
 SELECT ?organisation ?organisation2 
 WHERE 
 {
   ?organisation wdt:P31 wd:Q43229. #organisation (collective goal)
   ?organisation2 wdt:P31 wd:Q2029841. #organisation (economical concept)
 }
+"""
 
 #select location enitity 
-
+"""
 SELECT ?location ?locationLabel 
 WHERE 
 {
   ?location wdt:P31 wd:Q17334923. #where ?location isa location
 }
+"""

6fca2ef8da2969ef3fb67e58f61b4c6416955c39
FImhaouran <f.imhaouran@gmail.com>
2017-12-02 16:17:53 +0100

    Update SPARQL_mainfile.py

--- SPARQL_mainfile.py
+++ SPARQL_mainfile.py
@@ -128,6 +128,10 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX fbase: <http://rdf.freebase.com/ns/>
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wds: <http://www.wikidata.org/entity/statement/>
+PREFIX wdv: <http://www.wikidata.org/value/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
 """
 same_as_template = prefixes + """
 SELECT DISTINCT ?same WHERE {
